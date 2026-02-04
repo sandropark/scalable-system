973d8104c7080f47a9a2519cf85e2964dc3d115b
sandropark <birdob@gmail.com>
2026-02-04 16:58:37 +0900

    chore: DataInitializer 테스트 비활성화 및 JPA show-sql 설정 활성화

--- service/comment/src/test/java/com/sandro/service/comment/data/DataInitializer.java
+++ service/comment/src/test/java/com/sandro/service/comment/data/DataInitializer.java
@@ -4,6 +4,7 @@ import com.sandro.common.snowflake.Snowflake;
 import com.sandro.service.comment.domain.Comment;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +14,7 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+@Disabled
 @SpringBootTest
 public class DataInitializer {
     @PersistenceContext
